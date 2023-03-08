30f4c8d5c68913148aaae6028e45453fdff21c1e
sakeeo <ig.muhammadlim@gmail.com>
2023-03-08 14:37:24 +0800

    update app contact versi 2, menambahkan fitur add,hapus,getlist dan getdetail

--- contact-app-v2/app.js
+++ contact-app-v2/app.js
@@ -1,5 +1,11 @@
+const { demandCommand } = require("yargs");
 const yargs = require("yargs");
-const { simpanContact } = require("./constacts");
+const {
+  simpanContact,
+  getListContacts,
+  getDetailContact,
+  deleteContact,
+} = require("./constacts");
 
 yargs.command({
   command: "add",
@@ -22,13 +28,49 @@ yargs.command({
     },
   },
   handler(argv) {
-    // const contact = {
-    //   nama: argv.nama,
-    //   email: argv.email,
-    //   hp: argv.hp,
-    // };
-    // console.log(contact);
     simpanContact(argv.nama, argv.email, argv.hp);
+  },
+}),
+  demandCommand();
+
+// menampilkan list nama dan nomer hp contact
+yargs.command({
+  command: "getlist",
+  describe: "menampilkan list nama dan nomer hp",
+  handler() {
+    getListContacts();
+  },
+});
+
+//menampilkan detail contact
+yargs.command({
+  command: "getdetail",
+  describe: "menampilkan detail contact by nama",
+  builder: {
+    nama: {
+      describe: "masukkan nama",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    getDetailContact(argv.nama);
+  },
+});
+
+//menghapus contact
+yargs.command({
+  command: "delete",
+  describe: "menghapus contact by nama",
+  builder: {
+    nama: {
+      describe: "masukkan nama",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    deleteContact(argv.nama);
   },
 });
 
--- contact-app-v2/constacts.js
+++ contact-app-v2/constacts.js
@@ -14,11 +14,15 @@ if (!fs.existsSync(filePath)) {
   fs.writeFileSync(filePath, "[]", "utf-8");
 }
 
-const simpanContact = (nama, email, hp) => {
-  const contact = { nama, email, hp };
+const loadContacts = () => {
   const file = fs.readFileSync(filePath, "utf-8");
   const contacts = JSON.parse(file);
+  return contacts;
+};
 
+const simpanContact = (nama, email, hp) => {
+  const contact = { nama, email, hp };
+  const contacts = loadContacts();
   //cek duplikat
   const duplikat = contacts.find((contact) => contact.nama === nama);
   if (duplikat) {
@@ -27,7 +31,6 @@ const simpanContact = (nama, email, hp) => {
     );
     return false;
   }
-
   //validasi email
   if (email) {
     if (!validator.isEmail(email)) {
@@ -37,7 +40,6 @@ const simpanContact = (nama, email, hp) => {
       return false;
     }
   }
-
   //validasi nomer hp
   if (!validator.isMobilePhone(hp, "id-ID")) {
     console.log(
@@ -45,7 +47,6 @@ const simpanContact = (nama, email, hp) => {
     );
     return false;
   }
-
   contacts.push(contact);
   fs.writeFile(filePath, JSON.stringify(contacts), (err) => {
     if (err) throw err;
@@ -53,4 +54,50 @@ const simpanContact = (nama, email, hp) => {
   });
 };
 
-module.exports = { simpanContact };
+const getListContacts = () => {
+  const contacts = loadContacts();
+  console.log(chalk.blue.inverse.bold("daftar contact :"));
+  contacts.forEach((contact, i) => {
+    console.log(`${i + 1}. ${contact.nama} - ${contact.hp}`);
+  });
+};
+
+const getDetailContact = (findNama) => {
+  const contacts = loadContacts();
+  const contact = contacts.find(
+    (contact) => contact.nama.toLowerCase() === findNama.toLowerCase()
+  );
+  if (!contact) {
+    console.log(chalk.red.inverse.bold(`nama ${findNama} tidak ditemukan!`));
+    return false;
+  }
+  console.log(chalk.blue.inverse.bold("detail contact :"));
+  console.log(`nama : ${contact.nama}`);
+  console.log(`nomer hp : ${contact.hp}`);
+  if (contact.email) {
+    console.log(`email : ${contact.email}`);
+  }
+};
+
+//delete Contact
+const deleteContact = (findNama) => {
+  const contacts = loadContacts();
+  const newcontact = contacts.filter(
+    (contact) => contact.nama.toLowerCase() === findNama.toLowerCase()
+  );
+
+  if (!contacts.lenght === newcontact.lenght) {
+    console.log(chalk.red.inverse.bold(`nama ${findNama} tidak ditemukan!`));
+    return false;
+  }
+
+  fs.writeFileSync(filePath, JSON.stringify(newcontact));
+  console.log(chalk.green.inverse.bold(`nama ${findNama} berhasil dihapus!`));
+};
+
+module.exports = {
+  simpanContact,
+  getListContacts,
+  getDetailContact,
+  deleteContact,
+};
