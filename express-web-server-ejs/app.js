50ca02dc16c515de85e440386258e7ad942b213a
sakeeo <ig.muhammadlim@gmail.com>
2023-03-09 11:33:37 +0800

    add view engine ejs

---
+++ express-web-server-ejs/app.js
@@ -0,0 +1,27 @@
+const { render } = require("ejs");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("<h1>404</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
